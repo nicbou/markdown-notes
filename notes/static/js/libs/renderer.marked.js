4c375e5546928b975990ae96a91aa1bf29477121
Nicolas Bouliane <contact@nicolasbouliane.com>
2015-05-10 16:45:38 +0200

    Set links to have target=_blank

--- notes/static/js/libs/renderer.marked.js
+++ notes/static/js/libs/renderer.marked.js
@@ -27,3 +27,25 @@ customRenderer.latex = function(text){
     el.attr('data-formula', text);
     return el.prop('outerHTML');
 };
+
+//Override the link renderer to have target="_blank"
+customRenderer.link = function(href, title, text) {
+  if (this.options.sanitize) {
+    try {
+      var prot = decodeURIComponent(unescape(href))
+        .replace(/[^\w:]/g, '')
+        .toLowerCase();
+    } catch (e) {
+      return '';
+    }
+    if (prot.indexOf('javascript:') === 0) {
+      return '';
+    }
+  }
+  var out = '<a target="_blank" href="' + href + '"';
+  if (title) {
+    out += ' title="' + title + '"';
+  }
+  out += '>' + text + '</a>';
+  return out;
+};
