670b66035f5454482a167c7befd108b2ee7f46c1
Nicolas Bouliane <ext-nicolas.bouliane@here.com>
2015-05-15 17:30:57 +0200

    Switching some responsibility over to the note provider

--- notes/static/js/controllers/notes.js
+++ notes/static/js/controllers/notes.js
@@ -13,8 +13,7 @@ app.config(function($locationProvider, $routeProvider) {
 
 app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, $routeParams, $timeout, $location, $q, $document){
     //Used to keep track of the preview refresh and autosave delay
-    var saveTimeout = null;
-    var saveTimeoutDelay = 200; //200ms after edit before preview refresh
+    var saveTimeout, previewTimeout;
 
     $scope.codemirrorOptions = {
         dragDrop: false, //Disabled so the window receives the event
@@ -43,29 +42,17 @@ app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, 
     };
 
     $scope.cachedFormulas = {};
+    $scope.messages = [];
+    $scope.currentNoteIndex = -1;
 
-    //Get all the notes, including the current one, then setup the controller
-    $noteProvider.getAll().then(
-        function(notes){
-            $scope.notes = notes;
-
-            //Set the current note if an ID is specified
-            if($location.search().note){
-                $scope.load($location.search().note);
-            }
-            else if($scope.notes.length > 0){
-                //Load the latest note if possible
-                $scope.load($scope.notes[0].id);
-            }
-
-            //If no note is loaded, init will create and save one "on change"
-            init();
-        }
-    );
+    $scope.$on('noteProvider:notesReceived', function(evt) {
+        init();
+    }, true);
+    $scope.noteProvider = $noteProvider;
 
     //Controller setup
     function init(){
-        $scope.messages = [];
+        var currentNote = 0;
 
         //MatJax options (latex equations)
         MathJax.Hub.Config($scope.mathjaxOptions);
@@ -79,66 +66,77 @@ app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, 
             $scope.cachedFormulas[formula] = output;
         });
 
-        //Set a default currentNote if none is loaded
-        if(!$scope.currentNote){
-            $scope.currentNote = {
-                title: '',
-                content: '',
-                date_created: (new Date()).toISOString(),
-            };
+        //Load a first note (or create one if needed)
+        currentNote = parseInt($location.search('note'), 10);
+        if(currentNote){
+            $scope.load(currentNote);
         }
-        $scope.updatePreview();
+        else if($scope.noteProvider.notes.length === 0){
+            $scope.create(true);
+        }
+        else{
+            $scope.load($scope.noteProvider.notes[0].id);
+        }
 
-        //Update the preview on content/title changes
-        $scope.$watch('currentNote', function(newVal, oldVal, scope) {
-            //Refresh the preview 200ms after keyup
-            if(saveTimeout){
-                $timeout.cancel(saveTimeout);
-            }
-            saveTimeout = $timeout(function(){
-                if(newVal.title !== oldVal.title || newVal.content !== oldVal.content){
-                    if(newVal.id === oldVal.id || newVal.id === null){
-                        scope.save(scope.currentNote).then(function(){
-                            //Update the ID in the URL after saving the note
-                            $location.search('note', scope.currentNote.id);
-                        });
-                    }
-                    else{
-                        //Update the ID in the URL after a note switch/creation
-                        $location.search('note', scope.currentNote.id);
-                    }
-                    scope.updatePreview();
+        //Watch notes for changes
+        $scope.$watch(
+            function(){
+                if($scope.currentNoteIndex >= 0 && $scope.noteProvider.notes.length > 0){
+                    return $scope.noteProvider.notes[$scope.currentNoteIndex];
                 }
-            }, saveTimeoutDelay);
-        }, true);
+            },
+            function(newValue, oldValue){
+                //Save 1s after keyup
+                if(saveTimeout){
+                    $timeout.cancel(saveTimeout);
+                }
+                saveTimeout = $timeout(function(){
+                    if(newValue !== undefined) $noteProvider.save(newValue).then(function(){
+                        $location.search('note', newValue.id);
+                    });
+                }, 1000);
+
+                //Refresh the preview 200ms after keyup
+                if(previewTimeout){
+                    $timeout.cancel(previewTimeout);
+                }
+                previewTimeout = $timeout(function(){
+                    $scope.updatePreview();
+                }, 200);
+            },
+            true
+        );
     }
 
     //Create a note
-    $scope.create = function(){
-        //Create the note
-        $scope.currentNote = {
+    $scope.create = function(loadCreatedNote){
+        //Close the menu
+        $("#notes-menu, #btn-menu").removeClass("open");
+
+        $noteProvider.save({
             title: '',
             content: '',
             date_created: (new Date()).toISOString(),
-        };
-
-        //Close the menu
-        $("#notes-menu, #btn-menu").removeClass("open");
+        }).then(function(note){
+            if(loadCreatedNote) $scope.load($noteProvider.notes[0].id);
+        });
     };
 
     //Loads a note from the server
     $scope.load = function(noteId, hideMenu){
+        var noteFound = false;
         hideMenu = hideMenu === undefined ? true : false;
 
         //Load an existing note
-        for(var i=0; i<$scope.notes.length; i++){
-            if($scope.notes[i].id.toString() == noteId){
-                $scope.currentNote = $scope.notes[i];
+        for(var i=0; i<$scope.noteProvider.notes.length; i++){
+            if($scope.noteProvider.notes[i].id === noteId){
+                $scope.currentNoteIndex = i;
+                noteFound = true;
             }
         }
 
-        if(!$scope.currentNote){
-            $location.search('note', null);
+        if(!noteFound){
+            //TODO
         }
 
         if(hideMenu){
@@ -153,71 +151,47 @@ app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, 
         }
     };
 
-    //Saves a note, updates the ID in the querystring. This function is a promise, so you can call .save().then()
-    $scope.save = function(note){
-        var deferred = $q.defer();
-
-        note.title = note.title || "";
-
-        //Add the note to the menu if necessary
-        if(!$scope.currentNote.id){
-            $scope.notes.unshift($scope.currentNote);
-        }
-
-        //Save the note, update the date
-        $noteProvider.save(note).then(
-            function(){
-                deferred.resolve(note);
-            }
-        );
-
-        return deferred.promise;
-    };
-
     //Saves a note then exports it to .md
     $scope.export = function(note){
         var blob = new Blob([note.content], {type:'text/x-markdown'});
         var fileName = note.title.toSlug(100);
-        fileName = fileName.length == 0 ? "untitled note" : fileName;
+        fileName = fileName.length === 0 ? "untitled note" : fileName;
         saveAs(blob, fileName + '.md'); //Uses filesaver.js
     };
 
     //Ctrl + S shortcut to export files
     $document.bind('keydown', function(event) {
         if((event.ctrlKey || event.metaKey) && String.fromCharCode(event.which).toLowerCase()==='s') {
-            $scope.export($scope.currentNote);
+            $scope.export($scope.noteProvider.notes[$scope.currentNoteIndex]);
             return false;
         }
     });
 
     //Deletes a note from the list
     $scope.remove = function(note){
-        $noteProvider.remove(note.id);
-
-        //Remove the note from the list
-        $scope.notes.splice($scope.notes.indexOf(note), 1);
-
-        //Don't leave the user on an undeleted note
-        if(note.id === $scope.currentNote.id){
-            //Try loading the first note, or create one as a fallback
-            if($scope.notes.length > 0){
-                $scope.load($scope.notes[0].id, false);
-            }
-            else{
-                $scope.create();
-            }
+        //Don't leave the user on a deleted note
+        if(note.id === $scope.noteProvider.notes[$scope.currentNoteIndex].id){
+            $scope.load($scope.noteProvider.notes[0].id, false);
         }
+
+        $noteProvider.remove(note).then(function(){
+            if($scope.noteProvider.notes.length === 0){
+                $scope.create(true);
+            }
+        });
     };
 
     //Updates the preview window
     $scope.updatePreview = function(){
+        if($scope.noteProvider.notes[$scope.currentNoteIndex] === undefined) return;
+
         var outputWindow = $('#output');
 
         //Get the scroll position
         var scrollTop = outputWindow.scrollTop();
 
         //Convert the markup to HTML and update the preview
-        var content = $scope.currentNote.content;
+        var content = $scope.noteProvider.notes[$scope.currentNoteIndex].content;
 
         $('#preview').html(marked(
             content,
@@ -251,26 +225,6 @@ app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, 
         });
     };
 
-    //Drop uploads
-    $scope.imageDropped = function(){
-        //Save the note if it's new
-        if(!$scope.currentNote.id){
-            var message = {
-                'message':'Saving note...',
-                'class':'info'
-            };
-            $scope.messages.push(message);
-
-            $scope.save($scope.currentNote).then(function(){
-                $scope.messages.splice($scope.messages.indexOf(message), 1);
-                uploadImage();
-            });
-        }
-        else{
-            uploadImage();
-        }
-    };
-
     function uploadImage(){
 
         var message = {
@@ -280,7 +234,7 @@ app.controller('NotesCtrl', function NotesCtrl($scope, $noteProvider, Uploader, 
         $scope.messages.push(message);
 
         //Upload the image
-        Uploader.uploadImage($scope.uploadedFile, $scope.currentNote.id).then(
+        Uploader.uploadImage($scope.uploadedFile, $scope.currentNoteIndex).then(
             function(imageUrl){
                 var markdownImage = '![](' + imageUrl + ')';
 
--- notes/static/js/services/notes.js
+++ notes/static/js/services/notes.js
@@ -1,43 +1,50 @@
 //Syncs notes with the API
 angular.module('notes.service', ['ngResource', 'notes.config'])
-    .factory('$noteProvider', ['$http','DUMMY_API','$q', function($http, DUMMY_API, $q){
+    .factory('$noteProvider', ['$rootScope', '$http', 'DUMMY_API','$q', function($rootScope, $http, DUMMY_API, $q){
         var api_url = DUMMY_API ? '/api/v1/note-dummy/' : '/api/v1/note/';
 
-        function fakePromise(){
-            var deferred = $q.defer();
-            deferred.resolve();
-            return deferred.promise;
-        }
-        
-        var $noteProvider = function(data) {
-            angular.extend(this, data);
+        var $notesProvider = {
+            notes: [],
+            save: function(note) {
+                note.title = note.title || "";
+
+                if(DUMMY_API){
+                    //Fake save operation. Assign the note a random ID, return a promise
+                    note.id = note.id || Math.ceil(Math.random()*10000);
+                    return fakePromise();
+                }
+
+                var noteProvider = this;
+                return $http.post(api_url, note).success(function(returnedNote) {
+                    if(!note.id){
+                        noteProvider.notes.push(note);
+                    }
+                    note.id = returnedNote.id;
+
+                    $rootScope.$broadcast('noteProvider:noteUpdated', note);
+                });
+            },
+            remove: function(note) {
+                var index = this.notes.indexOf(note);
+
+                if(index >= 0){
+                    if(DUMMY_API) return fakePromise();
+
+                    var noteProvider = this;
+                    return $http.delete(api_url + note.id + '/').success(function(){
+                        noteProvider.notes.splice(index, 1);
+                    });
+                }
+
+                $rootScope.$broadcast('noteProvider:noteUpdated', note);
+            },
         };
 
-        $noteProvider.getAll = function() {
-            return $http.get(api_url).then(function(response) {
-                return response.data.objects;
-            });
-        };
+        //Load from the server
+        $http.get(api_url).then(function(response) {
+            $notesProvider.notes = response.data.objects;
+            $rootScope.$broadcast('noteProvider:notesReceived');
+        });
 
-        $noteProvider.save = function(note) {
-            if(DUMMY_API){
-                //Fake save operation. Assign the note a random ID, return a promise
-                note.id = note.id || Math.ceil(Math.random()*10000);
-                return fakePromise();
-            }
-
-            return $http.post(api_url, note).success(function(returnedNote) {
-                note.id = returnedNote.id;
-                note.date_updated = returnedNote.date_updated;
-            });
-        };
-
-        $noteProvider.remove = function(id) {
-            //Fake save operation on the dummy API
-            if(DUMMY_API) return fakePromise();
-
-            return $http.delete(api_url + id + '/');
-        };
-
-        return $noteProvider;
+        return $notesProvider;
     }]);
