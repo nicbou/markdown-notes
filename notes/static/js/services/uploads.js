41c761fd662a353e44c5f739c64430c7bcf0914c
Nicolas Bouliane <ext-nicolas.bouliane@here.com>
2015-05-15 17:30:57 +0200

    Switching some responsibility over to the note provider

--- notes/static/js/services/uploads.js
+++ notes/static/js/services/uploads.js
@@ -4,14 +4,14 @@ angular.module('uploads.service', ['ngResource'])
             angular.extend(this, data);
         };
 
-        Uploader.uploadImage = function(file, noteId) {
+        Uploader.uploadImage = function(file, note) {
             //Check file type
             if (!file || !file.type.match(/image.*/)) return;
 
             //Build the form data (file, note ID and CSRF token)
             var fd = new FormData();
             fd.append("image", file); // Append the file
-            fd.append("note", noteId);
+            fd.append("note", note.id);
 
             //Send the request
             return $http({
