4ecc9ada58a91bbe01c583fd573035381b3c0683
Nicolas Bouliane <ext-nicolas.bouliane@here.com>
2015-05-29 12:07:13 +0200

    Added dates to note list in admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from notes.models import Note
 
-admin.site.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'title',
+        'date_created',
+        'date_updated',
+        'deleted',
+    )
+
+admin.site.register(Note, NoteAdmin)
