df05c8e4b2ec77f2c5ce12fa7ab2999dddb783bf
Nicolas Bouliane <ext-nicolas.bouliane@here.com>
2015-10-22 17:36:26 +0200

    Added redirect for shortened sharing URL

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,17 @@
 from authentication.views import LoginRequiredViewMixin
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 
 class NotesView(TemplateView):
     template_name = "notes/app.html"
+
+
+class SharedNoteRedirectView(RedirectView):
+    permanent = False
+    query_string = False
+    pattern_name = 'index'
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse_lazy('index') + '#/' + kwargs.get('public_id')
