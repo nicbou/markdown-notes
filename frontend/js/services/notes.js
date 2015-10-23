add994ce15e6ccfba871a3c7a80087e101b6090f
Nicolas Bouliane <ext-nicolas.bouliane@here.com>
2015-10-23 10:25:41 +0200

    Fixed wrong API URL deleting all notes

--- frontend/js/services/notes.js
+++ frontend/js/services/notes.js
@@ -1,6 +1,6 @@
 //Syncs notes with the API
 angular.module('notes.service').factory('$notesService', ['$rootScope', '$http', 'DUMMY_API','$q', function($rootScope, $http, DUMMY_API, $q){
-    var notesUrl = DUMMY_API ? '/api/v1/note-dummy/' : '/api/v1/note/?format=json',
+    var notesUrl = DUMMY_API ? '/api/v1/note-dummy/' : '/api/v1/note/',
         sharedNoteUrl = '/api/v1/shared-note/';
 
     function fakePromise(){
@@ -23,7 +23,7 @@ angular.module('notes.service').factory('$notesService', ['$rootScope', '$http',
             }
 
             var notesService = this;
-            return $http.post(notesUrl, note).success(function(returnedNote) {
+            return $http.post(notesUrl + '?format=json', note).success(function(returnedNote) {
                 if(!note.id){
                     note.date_created = moment.utc(returnedNote.date_created).tz(timeZone).toJSON();
                     note.public_id = returnedNote.public_id;
@@ -40,7 +40,7 @@ angular.module('notes.service').factory('$notesService', ['$rootScope', '$http',
                 if(DUMMY_API) return fakePromise();
 
                 var notesService = this;
-                return $http.delete(notesUrl + note.id + '/').success(function(){
+                return $http.delete(notesUrl + note.id + '/?format=json').success(function(){
                     notesService.notes.splice(index, 1);
                 });
             }
@@ -48,6 +48,7 @@ angular.module('notes.service').factory('$notesService', ['$rootScope', '$http',
         fetchFromServer: function(publicNoteId){
             var notesService = this,
                 apiUrl = publicNoteId ? sharedNoteUrl + publicNoteId : notesUrl;
+                apiUrl = apiUrl + '?format=json';
             return $http.get(apiUrl).then(function(response) {
                 if(publicNoteId){
                     notesService.notes = [response.data];
@@ -56,7 +57,7 @@ angular.module('notes.service').factory('$notesService', ['$rootScope', '$http',
                     notesService.notes = response.data.objects;
                     notesService.notes.forEach(function(note){
                         note.date_created = moment.utc(note.date_created).tz(timeZone).toJSON();
-                        note.date_updated = moment.utc(note.date_updated).tz(timeZone).toJSON();
+                        note.date_updated = note.date_created;
                     });
                 }
             });
