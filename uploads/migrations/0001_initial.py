b51abc2fb4428a66042ebb83e1a4cf8e7c5d5262
Nicolas Bouliane <contact@nicolasbouliane.com>
2016-02-28 15:30:48 +0100

    Bumped the character limit for uploaded image titles

---
+++ uploads/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notes', '0004_auto_20151022_1517'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('image', models.ImageField(upload_to=b'/')),
+                ('note', models.ForeignKey(to='notes.Note')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
